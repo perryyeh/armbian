073dbf9ffbe55da794834041c35704f160bf4d89
Perry Yeh <i@yeh.sh>
2026-04-28 12:46:15 +0800

    修复：移除migrate分支下多余的空行

--- install.sh
+++ install.sh
@@ -2547,7 +2547,6 @@ while true; do
         21) install_ddnsgo ;;
         22) install_lucky ;;
         70) migrate_docker_datadir ;;
-
         71) optimize_docker_logs ;;
         72) optimize_journald_to_volatile ;;
         90) create_macvlan_bridge ;;
